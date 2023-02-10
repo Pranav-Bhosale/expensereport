bca3747c93e08f91e151b482bf895331b5e771e7
Pranav-Bhosale <pranavbhosale@sahaj.ai>
2023-02-10 13:57:20 +0530

    Add Tests for getExpenseName function

--- expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseTest.kt
+++ expensereport-kotlin/src/test/kotlin/com/nelkinda/training/ExpenseTest.kt
@@ -40,4 +40,31 @@ class ExpenseTest {
 
         assertEquals("X", expenseMarker)
     }
+
+    @Test
+    fun `should return "Dinner" as expense name For DINNER expense type`() {
+        val expense = Expense(ExpenseType.DINNER, 1001)
+
+        val expenseName = expense.getExpenseName()
+
+        assertEquals("Dinner", expenseName)
+    }
+
+    @Test
+    fun `should return "Car Rental" as expense name For CAR_RENTAL expense type`() {
+        val expense = Expense(ExpenseType.CAR_RENTAL, 1001)
+
+        val expenseName = expense.getExpenseName()
+
+        assertEquals("Car Rental", expenseName)
+    }
+
+    @Test
+    fun `should return "Breakfast" as expense name For BREAKFAST expense type`() {
+        val expense = Expense(ExpenseType.BREAKFAST, 1001)
+
+        val expenseName = expense.getExpenseName()
+
+        assertEquals("Breakfast", expenseName)
+    }
 }
